02ad950abbfb23b1dde26657ef54f99e23ce4f96
chanyeo <g24562011@mailst.cjcu.edu.tw>
2018-06-14 16:03:18 +0800

    transactionAdapter2

--- app/src/main/java/com/example/cjcu/test0531/MainActivity.java
+++ app/src/main/java/com/example/cjcu/test0531/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -13,15 +15,20 @@ import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private ListView list;
     private MyDBHelper helper;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        mylist();
+        //mylist();
+    myview();
     }
 
     private void mylist() {
@@ -37,7 +44,25 @@ public class MainActivity extends AppCompatActivity {
                 1);
         list.setAdapter(simpleCursorAdapter);
     }
-
+    private void myview(){
+        recyclerView = findViewById(R.id.recyclerview);
+        helper = MyDBHelper.getInstance(this);
+        Cursor c = helper.getReadableDatabase()
+                .query("exp",null,null,null,null,null,null);
+        List<Person>trans=new ArrayList<>();
+        if(c.moveToFirst()){
+    do{
+        Person p=new Person();
+        p.setCdate(c.getString(c.getColumnIndex("cdate")));
+        p.setInfo(c.getString(c.getColumnIndex("info")));
+        p.setAmount(c.getInt(c.getColumnIndex("amount")));
+        trans.add(p);
+    }while (c.moveToNext());
+        }
+        TransactionAdapter adapter=new TransactionAdapter(trans);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,8 +70,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        mylist();
-
+        //mylist();
+        myview();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
